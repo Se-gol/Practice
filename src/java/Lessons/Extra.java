274055a78436ea34f99b2790bfc1632ec3bbf817
Saar <saarzi2004@gmail.com>
2021-07-29 19:04:09 +0300

    Add Random to Extra

--- src/java/Lessons/Extra.java
+++ src/java/Lessons/Extra.java
@@ -1,5 +1,6 @@
 package Lessons;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Extra {
@@ -15,5 +16,17 @@ public class Extra {
         double decimalInput = in.nextDouble();
         String stringInput = in.next();
         char characterInput = in.next().charAt(0);
+
+        // Random:
+
+        Random rnd = new Random();
+
+        // random number:
+
+        int randInteger = rnd.nextInt(100); // will return a random number between 0 - 99
+        int randomSpecifiedRange = rnd.nextInt(12 - 10 + 1) + 12; // will return a random number between 10 - 12
+        int randomSpecifiedRange2 = rnd.nextInt(1000 - 100 + 1) + 100; // will return a random number between 100 - 1000. the formula for a specified range is rnd.nextInt(max - min + 1) + min
+        double randDouble = rnd.nextDouble(); // will return a random number between 0 - 1
+        boolean bool = rnd.nextBoolean(); // will return either true or false (it's a coin flip)
     }
 }
