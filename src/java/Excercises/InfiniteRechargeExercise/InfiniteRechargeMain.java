78617e6e8d7a119ded73803df9ea56578c2c8f39
Gaia Zano <gaya.zano11@gmail.com>
2021-08-03 16:09:52 +0300

    Create InfiniteRechargeMain

--- src/java/Excercises/InfiniteRechargeExercise/InfiniteRechargeMain.java
+++ src/java/Excercises/InfiniteRechargeExercise/InfiniteRechargeMain.java
@@ -2,6 +2,21 @@ package Excercises.InfiniteRechargeExercise;
 
 public class InfiniteRechargeMain {
     public static void main(String[] args) {
+        Alliance blueAlliance = new Alliance(
+                true,
+                new Robot(6, 7, 9, 6, 6, 5, true),
+                new Robot(8, 4, 5, 8, 4, 2, false),
+                new Robot(3, 9, 3, 2, 7, 3, false)
+        );
+
+        Alliance redAlliance = new Alliance(
+                false,
+                new Robot(5, 3, 8, 7, 2, 1, true),
+                new Robot(3, 9, 5, 3, 2, 4, true),
+                new Robot(3, 3, 6, 2, 1, 4, false)
+        );
+        Game game = new Game(blueAlliance, redAlliance);
+        game.result();
 
     }
 }
