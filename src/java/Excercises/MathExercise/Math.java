420ecfd5a4165a719c913f5840f25b1441ac1930
Yuval Laifenfeld <yuvallaif@gmail.com>
2021-10-29 13:30:51 +0300

    Math exercise

--- src/java/Excercises/MathExercise/Math.java
+++ src/java/Excercises/MathExercise/Math.java
@@ -1,9 +1,96 @@
 package Excercises.MathExercise;
 
 public class Math {
-    final double Pi = java.lang.Math.PI;
+    final double Pi = 3.141592653589793238;
 
-    public static double getSum(double[] array) {
-        return 0;
+
+    public static int Sum(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        return sum;
     }
+
+    public static double Average(double[] nums) {
+        double num2 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            num2 += nums[i];
+        }
+        double average = num2 / nums.length;
+        return average;
+    }
+
+    public static double Product(int[] nums) {
+        int product = 0;
+        for (int i = 0; i < nums.length; i++) {
+            product *= nums[i];
+        }
+        return product;
+    }
+
+    public static double Max(int[] nums) {
+        int num2 = 0;
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            num2 = nums[i];
+            if (max > num2)
+                max = max;
+            else if (num2 > max)
+                max = num2;
+            else
+                max = max;
+        }
+        return max;
+    }
+
+    public static double Min(int[] nums) {
+        int num2 = 0;
+        double min = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            num2 = nums[i];
+            if (min < num2)
+                min = min;
+            else if (num2 < min)
+                min = num2;
+            else
+                min = min;
+        }
+        return min;
+    }
+
+    public static double Pow(int num, int numOfTimes) {
+        int pow = 0;
+        int[] nums = new int[numOfTimes - 1];
+        for (int i = 0; i < nums.length; i++) {
+            num *= num;
+        }
+        pow = num;
+        return pow;
+    }
+
+    public static double Factorial(int numOfTimes) {
+        int blah = 1;
+        int factprialFunction = 1;
+        int[] nums = new int[numOfTimes];
+        for (int i = 1; i < nums.length; i++) {
+            blah = i + 1;
+            factprialFunction *= blah;
+        }
+        return factprialFunction;
+    }
+
+    public static int Fibonacci(int numOfTimes) {
+        int[] nums = new int[numOfTimes - 1];
+        int ActiveNum = 1;
+        int PastNum = 0;
+        for (int i = 0; i < nums.length; i++){
+            int TemporaryNum = ActiveNum;
+            ActiveNum = ActiveNum + PastNum;
+            PastNum = TemporaryNum;
+        }
+        return PastNum;
+    }
+
+    public static int
 }
--- src/java/Excercises/MathExercise/MathMain.java
+++ src/java/Excercises/MathExercise/MathMain.java
@@ -1,7 +1,11 @@
 package Excercises.MathExercise;
 
+import java.lang.reflect.Array;
+
 public class MathMain {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println(Math.Pow(10,2));
+        System.out.println(Math.Factorial(1));
+        System.out.println(Math.Fibonacci(10));
     }
 }
